6d69ddcc9801cc4c5dc3285f4898d11ff200fb2e
snowykte0426 <snowykte0426@naver.com>
2025-08-25 23:37:26 +0900

    add: Swagger 구성 추가

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/config/SwaggerConfig.kt
@@ -0,0 +1,59 @@
+package team.themoment.hellogsmv3.global.config
+
+import com.team.incube.gsmc.v3.global.common.response.CommonApiResponse
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.Operation
+import io.swagger.v3.oas.models.media.Schema
+import org.springdoc.core.customizers.OperationCustomizer
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@OpenAPIDefinition(
+    info = Info(
+        title = "GSMC V3",
+        description = "GSM 인증제 관리 서비스",
+        version = "v3"
+    )
+)
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun api(operationCustomizer: OperationCustomizer): GroupedOpenApi =
+        GroupedOpenApi.builder()
+            .group("GSMC V3 API")
+            .pathsToMatch("/api/v3/**")
+            .addOperationCustomizer(operationCustomizer)
+            .build()
+
+    @Bean
+    fun operationCustomizer(): OperationCustomizer =
+        OperationCustomizer { operation, handlerMethod ->
+            val returnType = handlerMethod.method.returnType
+            val hideDataField = CommonApiResponse::class.java.isAssignableFrom(returnType)
+            addResponseBodyWrapperSchemaExample(operation, hideDataField)
+
+            operation
+        }
+
+    private fun addResponseBodyWrapperSchemaExample(operation: Operation, hideDataField: Boolean) {
+        operation.responses["200"]?.content?.let { content ->
+            content.forEach { (_, mediaType) ->
+                val originalSchema = mediaType.schema
+                mediaType.schema = wrapSchema(originalSchema, hideDataField)
+            }
+        }
+    }
+
+    private fun wrapSchema(originalSchema: Schema<*>?, hideDataField: Boolean): Schema<*> =
+        Schema<Any>().apply {
+            addProperty("status", Schema<String>().type("string").example("OK"))
+            addProperty("code", Schema<Int>().type("integer").example(200))
+            addProperty("message", Schema<String>().type("string").example("OK"))
+            if (!hideDataField) {
+                addProperty("data", originalSchema)
+            }
+        }
+}
