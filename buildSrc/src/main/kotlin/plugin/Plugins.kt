dc74f95313385975a185bc1f0ee2ab9885d0e667
snowykte0426 <snowykte0426@naver.com>
2025-08-25 19:59:37 +0900

    update: 플러그인 관리 `buildSrc`에 추가

---
+++ buildSrc/src/main/kotlin/plugin/Plugins.kt
@@ -0,0 +1,12 @@
+package plugin
+
+object Plugins {
+    const val SPRING_BOOT = "org.springframework.boot"
+    const val DEPENDENCY_MANAGEMENT = "io.spring.dependency-management"
+    const val KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
+    const val KOTLIN_SPRING = "org.jetbrains.kotlin.plugin.spring"
+    const val KOTLIN_JPA = "org.jetbrains.kotlin.plugin.jpa"
+    const val KOTLIN_ALLOPEN = "org.jetbrains.kotlin.plugin.allopen"
+    const val KAPT = "org.jetbrains.kotlin.kapt"
+    const val KOTEST = "io.kotest"
+}
