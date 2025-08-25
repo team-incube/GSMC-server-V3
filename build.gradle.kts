07d61ce6e12685c598807f97d06a1e12b4566507
snowykte0426 <snowykte0426@naver.com>
2025-08-25 20:01:33 +0900

    update: `AppOpen` 플러그인 추가로 인한 중복 블럭 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,12 +59,6 @@ kotlin {
     }
 }
 
-allOpen {
-    annotation("jakarta.persistence.Entity")
-    annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
-}
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
