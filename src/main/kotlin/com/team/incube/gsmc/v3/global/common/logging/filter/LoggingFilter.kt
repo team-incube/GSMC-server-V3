68262a0b3ed75382f7ff89c3feef1e3900e2bcf6
snowykte0426 <snowykte0426@naver.com>
2025-08-26 00:11:52 +0900

    update: 요청/응답 로깅 필터에서 미사용 의존성 제거

--- src/main/kotlin/com/team/incube/gsmc/v3/global/common/logging/filter/LoggingFilter.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/common/logging/filter/LoggingFilter.kt
@@ -6,7 +6,6 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.util.AntPathMatcher
 import org.springframework.util.StringUtils
