67f9290468b8d244763c26635f64a9cfc5474c96
snowykte0426 <snowykte0426@naver.com>
2025-08-27 17:10:42 +0900

    add: 핸들링 되지 않은 예외 발생 시 Discord 메세지 전송기능 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/global/common/error/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/common/error/handler/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package com.team.incube.gsmc.v3.global.common.error.handler
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.team.incube.gsmc.v3.global.common.error.discord.DiscordErrorNotificationService
 import com.team.incube.gsmc.v3.global.common.error.exception.FeignClientException
 import com.team.incube.gsmc.v3.global.common.error.exception.GsmcException
 import com.team.incube.gsmc.v3.global.common.response.data.CommonApiResponse
@@ -11,13 +12,17 @@ import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
 import org.springframework.web.multipart.MaxUploadSizeExceededException
 import org.springframework.web.servlet.NoHandlerFoundException
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @EnableWebMvc
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler(
+    private val discordErrorNotificationService: DiscordErrorNotificationService? = null,
+) {
     companion object {
         private val objectMapper = ObjectMapper()
     }
@@ -69,9 +74,20 @@ class GlobalExceptionHandler {
         return CommonApiResponse.error(ex.message ?: "Feign 클라이언트 오류", ex.status)
     }
 
-    @ExceptionHandler(RuntimeException::class)
-    fun handleUnexpectedException(ex: RuntimeException): CommonApiResponse<Nothing> {
-        logger().error("UnexpectedException Occur : ", ex)
+    @ExceptionHandler(Exception::class)
+    fun handleUnexpectedException(ex: Exception): CommonApiResponse<Nothing> {
+        logger().error("Unhandled Exception Occur : ", ex)
+
+        discordErrorNotificationService?.notifyError(
+            exception = ex,
+            context = "핸들링되지 않은 예외 발생",
+            additionalInfo =
+                mapOf(
+                    "Exception Type" to (ex::class.simpleName ?: "Unknown"),
+                    "Thread" to Thread.currentThread().name,
+                    "Request URI" to getCurrentRequestUri(),
+                ),
+        )
         return CommonApiResponse.error(
             message = "서버 내부 오류가 발생했습니다.",
             status = HttpStatus.INTERNAL_SERVER_ERROR,
@@ -113,4 +129,12 @@ class GlobalExceptionHandler {
                 "Validation failed"
             }
     }
+
+    private fun getCurrentRequestUri(): String =
+        try {
+            val requestAttributes = RequestContextHolder.getRequestAttributes() as? ServletRequestAttributes
+            requestAttributes?.request?.requestURI ?: "Unknown"
+        } catch (e: Exception) {
+            "Unable to get request URI"
+        }
 }
