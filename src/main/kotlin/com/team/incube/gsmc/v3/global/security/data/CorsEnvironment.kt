341225279c953d2128d27d3d7787b779fb1b5708
snowykte0426 <snowykte0426@naver.com>
2025-08-27 11:48:05 +0900

    update: 형식에 알맞도록 CORS 관련 프로퍼티 위치 조정

--- src/main/kotlin/com/team/incube/gsmc/v3/global/security/data/CorsEnvironment.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/security/data/CorsEnvironment.kt
@@ -2,7 +2,7 @@ package com.team.incube.gsmc.v3.global.security.data
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "security.cors")
+@ConfigurationProperties(prefix = "spring.security.cors")
 data class CorsEnvironment(
     val allowedOrigins: List<String> = listOf("*"),
 )
