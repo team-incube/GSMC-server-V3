02b14506bf6061a7566197c9344e099d473ecdd0
snowykte0426 <snowykte0426@naver.com>
2025-09-09 15:29:04 +0900

    add: 시간대 설정 클래스 구현

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/config/TimeZoneConfig.kt
@@ -0,0 +1,17 @@
+package com.team.incube.gsmc.v3.global.config
+
+import jakarta.annotation.PostConstruct
+import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+import java.util.TimeZone
+
+@Configuration
+class TimeZoneConfig {
+
+    @PostConstruct
+    fun init() {
+        System.setProperty("user.timezone", "Asia/Seoul")
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+        logger().info("Default timezone set to Asia/Seoul: {}", LocalDateTime.now())
+    }
+}
