c2f8a9d8380b70c8357d2ac04a2c119ea69fc287
snowykte0426 <snowykte0426@naver.com>
2025-10-01 23:04:40 +0900

    add: 파일 엔티티에 대응되는 DTO 생성

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/file/dto/File.kt
@@ -0,0 +1,8 @@
+package com.team.incube.gsmc.v3.domain.file.dto
+
+data class File(
+    val fileId: Long?,
+    val fileOriginalName: String?,
+    val fileStoredName: String?,
+    val fileUri: String?,
+)
