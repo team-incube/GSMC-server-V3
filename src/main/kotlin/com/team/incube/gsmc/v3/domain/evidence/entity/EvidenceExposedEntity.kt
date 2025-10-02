c3f105448dffde838d7ec318b221cebdd2b547fc
snowykte0426 <snowykte0426@naver.com>
2025-10-02 11:17:37 +0900

    add: 증빙자료 DTO 정의

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/evidence/entity/EvidenceExposedEntity.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/evidence/entity/EvidenceExposedEntity.kt
@@ -6,7 +6,7 @@ import java.time.Instant
 
 object EvidenceExposedEntity : Table(name = "tb_evidence") {
     val id = long(name = "evidence_id").autoIncrement()
-    val title = varchar(name = "evidence_title", length = 255).nullable()
+    val title = varchar(name = "evidence_title", length = 255)
     val content = text(name = "evidence_content")
     val createdAt = timestamp(name = "evidence_created_at").default(Instant.now())
     val updatedAt = timestamp(name = "evidence_updated_at").default(Instant.now())
