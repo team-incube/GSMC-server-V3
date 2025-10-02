b157c4e112404cfa5d90c3927e954a697ee13009
snowykte0426 <snowykte0426@naver.com>
2025-10-02 11:18:28 +0900

    add: 증빙자료 단건 조회 API 구현

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/evidence/presentation/EvidenceController.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/evidence/presentation/EvidenceController.kt
@@ -1,5 +1,8 @@
 package com.team.incube.gsmc.v3.domain.evidence.presentation
 
+import com.team.incube.gsmc.v3.domain.evidence.presentation.data.response.GetEvidenceResponse
+import com.team.incube.gsmc.v3.domain.evidence.service.FindEvidenceByIdService
+import com.team.incube.gsmc.v3.global.common.response.data.CommonApiResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v3/evidences")
-class EvidenceController()
-{
+class EvidenceController(
+    private val findEvidenceByIdService: FindEvidenceByIdService,
+) {
     @GetMapping("/{evidenceId}")
-    fun getEvidence(@PathVariable evidenceId: String): Nothing = TODO()
+    fun getEvidence(
+        @PathVariable evidenceId: Long,
+    ): CommonApiResponse<GetEvidenceResponse> {
+        val evidence = findEvidenceByIdService.execute(evidenceId)
+        return CommonApiResponse.success("증빙자료 조회가 완료되었습니다.", evidence)
+    }
 }
