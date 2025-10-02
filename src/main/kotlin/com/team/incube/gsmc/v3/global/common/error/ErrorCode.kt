6d6c81bbf2cc501a09552f0424d998509158ec9b
snowykte0426 <snowykte0426@naver.com>
2025-10-02 11:17:22 +0900

    add: 증빙자료 찾지 못함 예외 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/global/common/error/ErrorCode.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/common/error/ErrorCode.kt
@@ -3,4 +3,7 @@ package com.team.incube.gsmc.v3.global.common.error
 enum class ErrorCode(
     val message: String,
     val status: Int,
-)
+) {
+    // Evidence
+    EVIDENCE_NOT_FOUND("해당 증빙자료를 찾을 수 없습니다.", 404),
+}
