71af7d5b9cc91610e4c4bd373a3e64e9bb99ef90
snowykte0426 <snowykte0426@naver.com>
2025-10-02 09:04:08 +0900

    add: 증빙자료 단건 조회 서비스 클래스 정의

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/evidence/service/FindEvidenceByIdService.kt
@@ -0,0 +1,5 @@
+package com.team.incube.gsmc.v3.domain.evidence.service
+
+interface FindEvidenceByIdService {
+    fun execute(evidenceId: Long)
+}
---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/evidence/service/impl/FindEvidenceByIdServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.team.incube.gsmc.v3.domain.evidence.service.impl
+
+import org.springframework.stereotype.Service
+
+@Service
+class FindEvidenceByIdServiceImpl {
+
+}
