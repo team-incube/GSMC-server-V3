702b37c5111df51f2be6e8b332b610f07c8954a5
snowykte0426 <snowykte0426@naver.com>
2025-10-02 11:17:57 +0900

    add: 증빙자료 단건 조회 서비스 클래스 구현

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/evidence/service/FindEvidenceByIdService.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/evidence/service/FindEvidenceByIdService.kt
@@ -1,5 +1,7 @@
 package com.team.incube.gsmc.v3.domain.evidence.service
 
+import com.team.incube.gsmc.v3.domain.evidence.presentation.data.response.GetEvidenceResponse
+
 interface FindEvidenceByIdService {
-    fun execute(evidenceId: Long)
+    fun execute(evidenceId: Long): GetEvidenceResponse
 }
