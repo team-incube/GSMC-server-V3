dd9051b028f26ecfc77bb7cc89bdbc45c353a850
snowykte0426 <snowykte0426@naver.com>
2025-10-04 22:39:57 +0900

    update: 이중으로 선언된 트랜잭션 블록 제거

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/file/repository/impl/FileExposedRepositoryImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/file/repository/impl/FileExposedRepositoryImpl.kt
@@ -3,18 +3,14 @@ package com.team.incube.gsmc.v3.domain.file.repository.impl
 import com.team.incube.gsmc.v3.domain.file.entity.FileExposedEntity
 import com.team.incube.gsmc.v3.domain.file.repository.FileExposedRepository
 import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Repository
 
 @Repository
 class FileExposedRepositoryImpl : FileExposedRepository {
     override fun existsByIdIn(fileIds: List<Long>): Boolean =
-        transaction {
-            val existingFileIds =
-                FileExposedEntity
-                    .selectAll()
-                    .where { FileExposedEntity.id inList fileIds }
-                    .map { it[FileExposedEntity.id] }
-            existingFileIds.size == fileIds.size
-        }
+        FileExposedEntity
+            .selectAll()
+            .where { FileExposedEntity.id inList fileIds }
+            .map { it[FileExposedEntity.id] }
+            .size == fileIds.size
 }
