484e93c7c70c6cc6b95a6edad230b12247440460
snowykte0426 <snowykte0426@naver.com>
2025-10-05 23:22:34 +0900

    add: AWS S3 관련 작업 클래스 선언

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/thirdparty/aws/s3/config/S3Config.kt
@@ -0,0 +1,17 @@
+package com.team.incube.gsmc.v3.global.thirdparty.aws.s3.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+
+@Configuration
+class S3Config {
+
+    @Bean
+    fun s3Client(): S3Client {
+        return S3Client.builder()
+            .region(Region.AP_NORTHEAST_2)
+            .build()
+    }
+}
