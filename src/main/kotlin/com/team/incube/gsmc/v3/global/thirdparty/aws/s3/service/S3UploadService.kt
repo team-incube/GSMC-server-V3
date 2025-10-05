ffa51c1bfc214afc64d9fd96975a047af07aba3c
snowykte0426 <snowykte0426@naver.com>
2025-10-05 23:59:00 +0900

    update: S3 업로드/삭제 시 `S3Template` 사용하도록 변경

--- src/main/kotlin/com/team/incube/gsmc/v3/global/thirdparty/aws/s3/service/S3UploadService.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/thirdparty/aws/s3/service/S3UploadService.kt
@@ -2,7 +2,6 @@ package com.team.incube.gsmc.v3.global.thirdparty.aws.s3.service
 
 import org.springframework.web.multipart.MultipartFile
 
-interface S3Service {
-    fun uploadFile(file: MultipartFile): String
-    fun deleteFile(fileUri: String)
+interface S3UploadService {
+    fun execute(file: MultipartFile): String
 }
