284813f14fb3953f68fc4fdcc250178f5af9cfb4
snowykte0426 <snowykte0426@naver.com>
2025-10-14 20:50:51 +0900

    add: 인증제 점수 상태 수정,삭제 엔드포인트 정의

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/ScoreController.kt
@@ -0,0 +1,73 @@
+package com.team.incube.gsmc.v3.domain.score.presentation
+
+import com.team.incube.gsmc.v3.domain.score.presentation.data.request.UpdateScoreStatusRequest
+import com.team.incube.gsmc.v3.domain.score.service.DeleteScoreService
+import com.team.incube.gsmc.v3.domain.score.service.UpdateScoreStatusService
+import com.team.incube.gsmc.v3.global.common.response.data.CommonApiResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v3/scores")
+class ScoreController(
+    private val updateScoreStatusService: UpdateScoreStatusService,
+    private val deleteScoreService: DeleteScoreService,
+) {
+    @Operation(summary = "인증제 점수 상태 업데이트", description = "인증제 점수의 승인/거절 상태를 업데이트합니다")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "요청이 성공함",
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "존재하지 않는 인증제 점수를 매핑함",
+                content = [Content()],
+            ),
+        ],
+    )
+    @SecurityRequirement(name = "bearerAuth")
+    @PutMapping("/{scoreId}/status")
+    fun updateEvidenceStatus(
+        @PathVariable scoreId: Long,
+        @Valid @RequestBody request: UpdateScoreStatusRequest,
+    ): CommonApiResponse<Nothing> {
+        updateScoreStatusService.execute(scoreId, request.scoreStatus)
+        return CommonApiResponse.success("OK")
+    }
+
+    @Operation(summary = "인증제 점수 삭제", description = "인증제 점수 및 연관된 증빙자료와 파일을 삭제합니다")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "요청이 성공함",
+                content = [Content()],
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "존재하지 않는 점수를 매핑함",
+                content = [Content()],
+            ),
+        ],
+    )
+    @SecurityRequirement(name = "bearerAuth")
+    @DeleteMapping("/{scoreId}")
+    fun deleteScore(
+        @PathVariable scoreId: Long,
+    ): CommonApiResponse<Nothing> {
+        deleteScoreService.execute(scoreId)
+        return CommonApiResponse.success("OK")
+    }
+}
