94dd9d98de5873d98b958322dd6857f47ce22d29
C0ng_yun <s25069@gsm.hs.kr>
2025-11-05 23:32:17 +0900

    update: GitHub Actions 포맷 체크 통과를 위한 공백 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/member/repository/MemberExposedRepository.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/member/repository/MemberExposedRepository.kt
@@ -4,6 +4,8 @@ import com.team.incube.gsmc.v3.domain.member.dto.Member
 import com.team.incube.gsmc.v3.domain.member.presentation.data.request.FindMemberRequest
 
 interface MemberExposedRepository {
+
     fun findMembers(query: FindMemberRequest): List<Member>
+
     fun findById(memberId: Long): Member?
 }
