5edd18a4af3ce9a7c7f93467791c1418acfedcdb
C0ng_yun <s25069@gsm.hs.kr>
2025-11-05 23:30:17 +0900

    update: MemberExposedRepository 불 필요한 공백 제거

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/member/repository/MemberExposedRepository.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/member/repository/MemberExposedRepository.kt
@@ -4,8 +4,6 @@ import com.team.incube.gsmc.v3.domain.member.dto.Member
 import com.team.incube.gsmc.v3.domain.member.presentation.data.request.FindMemberRequest
 
 interface MemberExposedRepository {
-
     fun findMembers(query: FindMemberRequest): List<Member>
-
     fun findById(memberId: Long): Member?
 }
