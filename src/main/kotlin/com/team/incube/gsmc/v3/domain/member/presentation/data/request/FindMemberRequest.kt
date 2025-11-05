a0b0284622074f46e1fede4f619d20ab36487e23
C0ng_yun <s25069@gsm.hs.kr>
2025-11-05 22:53:24 +0900

    add: 사용자 정보 검색 시 요청  DTO 정의

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/member/presentation/data/request/FindMemberRequest.kt
@@ -0,0 +1,16 @@
+package com.team.incube.gsmc.v3.domain.member.presentation.data.request
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.team.incube.gsmc.v3.domain.member.dto.constant.MemberRole
+
+data class FindMemberRequest(
+    val email: String? = null,
+    val name: String? = null,
+    val role: MemberRole? = null,
+    val grade: Int? = null,
+    @JsonProperty("class")
+    val classNumber: Int? = null,
+    val number: Int? = null,
+    val maxScore: Int? = null,
+    val minScore: Int? = null,
+)
