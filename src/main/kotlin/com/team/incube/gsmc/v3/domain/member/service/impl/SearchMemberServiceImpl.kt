00dfb23a83865481ecdc461e76df4027f6e8849e
C0ng_yun <s25069@gsm.hs.kr>
2025-11-07 15:17:52 +0900

    update: 파라미터 변수명 변경

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/member/service/impl/SearchMemberServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/member/service/impl/SearchMemberServiceImpl.kt
@@ -5,15 +5,17 @@ import com.team.incube.gsmc.v3.domain.member.presentation.data.response.SearchMe
 import com.team.incube.gsmc.v3.domain.member.repository.MemberExposedRepository
 import com.team.incube.gsmc.v3.domain.member.service.SearchMemberService
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
 class SearchMemberServiceImpl(
     private val memberExposedRepository: MemberExposedRepository,
 ) : SearchMemberService {
-    override fun execute(memberId: SearchMemberRequest): List<SearchMemberResponse> =
+    override fun execute(request: SearchMemberRequest, pageable: Pageable): Page<SearchMemberResponse> =
         transaction {
-            val member = memberExposedRepository.findMembers(memberId)
-            SearchMemberResponse.fromList(member)
+            val member = memberExposedRepository.findMembers(request, pageable)
+            member.map { SearchMemberResponse.from(it) }
         }
 }
