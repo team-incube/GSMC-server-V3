a975675444e89b94b06aba706e1a73de80ca172d
hej090224 <fc49854985@gmail.com>
2025-11-08 19:21:14 +0900

    add: 개발자용 회원 탈퇴 서비스 구현

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/developer/service/WithdrawMemberService.kt
@@ -0,0 +1,5 @@
+package com.team.incube.gsmc.v3.domain.developer.service
+
+interface WithdrawMemberService {
+    fun execute(email: String)
+}
---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/developer/service/impl/WithdrawMemberServiceImpl.kt
@@ -0,0 +1,23 @@
+package com.team.incube.gsmc.v3.domain.developer.service.impl
+
+import com.team.incube.gsmc.v3.domain.developer.repository.DeveloperExposedRepository
+import com.team.incube.gsmc.v3.domain.developer.service.WithdrawMemberService
+import com.team.incube.gsmc.v3.global.common.error.ErrorCode
+import com.team.incube.gsmc.v3.global.common.error.exception.GsmcException
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.stereotype.Service
+
+@Service
+class WithdrawMemberServiceImpl(
+    private val developerExposedRepository: DeveloperExposedRepository,
+) : WithdrawMemberService {
+
+    override fun execute(email: String) {
+        transaction {
+            val deleted = developerExposedRepository.deleteMemberByEmail(email)
+            if (deleted == 0) {
+                throw GsmcException(ErrorCode.MEMBER_NOT_FOUND)
+            }
+        }
+    }
+}
