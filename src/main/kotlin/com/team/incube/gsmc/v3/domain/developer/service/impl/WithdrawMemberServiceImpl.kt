b46ea9a51f9e3c684c6d3682c6244d75c687efff
hej090224 <fc49854985@gmail.com>
2025-11-10 12:18:58 +0900

    refactor: Developer API 서비스 로깅 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/developer/service/impl/WithdrawMemberServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/developer/service/impl/WithdrawMemberServiceImpl.kt
@@ -5,18 +5,25 @@ import com.team.incube.gsmc.v3.domain.developer.service.WithdrawMemberService
 import com.team.incube.gsmc.v3.global.common.error.ErrorCode
 import com.team.incube.gsmc.v3.global.common.error.exception.GsmcException
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class WithdrawMemberServiceImpl(
     private val developerExposedRepository: DeveloperExposedRepository,
 ) : WithdrawMemberService {
+    private val log = LoggerFactory.getLogger(WithdrawMemberServiceImpl::class.java)
+
     override fun execute(email: String) {
         transaction {
             val deleted = developerExposedRepository.deleteMemberByEmail(email)
+
             if (deleted == 0) {
+                log.info("Developer member withdrawal failed: member not found. email={}", email)
                 throw GsmcException(ErrorCode.MEMBER_NOT_FOUND)
             }
+
+            log.info("Developer member withdrawn successfully. email={}", email)
         }
     }
 }
