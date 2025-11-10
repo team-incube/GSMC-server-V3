d0eee68685e87d0812226fc2febbe668aafc7b80
hej090224 <fc49854985@gmail.com>
2025-11-10 12:19:20 +0900

    feat: Developer 권한 변경 요청 DTO 추가

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/developer/presentation/data/request/ChangeMemberRoleRequest.kt
@@ -0,0 +1,14 @@
+package com.team.incube.gsmc.v3.domain.developer.presentation.data.request
+
+import com.team.incube.gsmc.v3.domain.member.dto.constant.MemberRole
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+data class ChangeMemberRoleRequest(
+    @field:NotBlank
+    @field:Email(message = "유효한 이메일 형식이 아닙니다.")
+    val email: String,
+    @field:NotNull
+    val role: MemberRole,
+)
