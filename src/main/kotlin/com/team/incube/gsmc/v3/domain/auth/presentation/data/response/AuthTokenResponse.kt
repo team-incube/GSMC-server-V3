68a8ac06635baf80303911eb7120e2b2a5502585
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-12 15:47:32 +0900

    update: LocalDateTime에 맞는 예시로 수정

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/presentation/data/response/AuthTokenResponse.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/presentation/data/response/AuthTokenResponse.kt
@@ -7,11 +7,11 @@ import java.time.LocalDateTime
 data class AuthTokenResponse(
     @field:Schema(description = "Access Token")
     val accessToken: String,
-    @field:Schema(description = "Access Token 유효기간", example = "2025-11-12T10:12:12.123Z")
+    @field:Schema(description = "Access Token 유효기간", example = "2025-11-12T10:12:12.123")
     val accessTokenExpiresAt: LocalDateTime,
     @field:Schema(description = "Refresh Token")
     val refreshToken: String,
-    @field:Schema(description = "Refresh Token 유효기간", example = "2025-11-19T10:12:12.123Z")
+    @field:Schema(description = "Refresh Token 유효기간", example = "2025-11-19T10:12:12.123")
     val refreshTokenExpiresAt: LocalDateTime,
     @field:Schema(description = "사용자 권한")
     val role: MemberRole,
