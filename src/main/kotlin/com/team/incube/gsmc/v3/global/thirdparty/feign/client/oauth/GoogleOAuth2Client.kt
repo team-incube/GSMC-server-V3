edd6386824372ec78aa601d444f93f2bce68b940
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-12 14:08:25 +0900

    update: RequestBody로 수정

--- src/main/kotlin/com/team/incube/gsmc/v3/global/thirdparty/feign/client/oauth/GoogleOAuth2Client.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/thirdparty/feign/client/oauth/GoogleOAuth2Client.kt
@@ -4,7 +4,7 @@ import com.team.incube.gsmc.v3.global.thirdparty.feign.data.response.GoogleToken
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
 
 @FeignClient(
     name = "google-oauth2-client",
@@ -13,10 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam
 interface GoogleOAuth2Client {
     @PostMapping(value = ["/token"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     fun exchangeCodeForToken(
-        @RequestParam("grant_type") grantType: String = "authorization_code",
-        @RequestParam("client_id") clientId: String,
-        @RequestParam("client_secret") clientSecret: String,
-        @RequestParam("code") code: String,
-        @RequestParam("redirect_uri") redirectUri: String,
+        @RequestBody form: Map<String, String>,
     ): GoogleTokenResponse
 }
