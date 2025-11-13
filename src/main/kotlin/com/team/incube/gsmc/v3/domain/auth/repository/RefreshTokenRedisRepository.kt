0f929b67e64f7d42310c93d021d99a7643bfea48
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-13 12:45:50 +0900

    update: 불필요한 메서드 제거

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/repository/RefreshTokenRedisRepository.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/repository/RefreshTokenRedisRepository.kt
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface RefreshTokenRedisRepository : CrudRepository<RefreshTokenRedisEntity, String> {
-    fun deleteByToken(token: String)
 }
