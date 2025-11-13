ba456b06fe0da39b03e396a0053b95f3ff5ac054
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-13 16:12:41 +0900

    add: CustomAuthenticationEntryPoint 추가

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/security/handler/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,26 @@
+package com.team.incube.gsmc.v3.global.security.handler
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.team.incube.gsmc.v3.global.common.response.data.CommonApiResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAuthenticationEntryPoint(
+    private val objectMapper: ObjectMapper,
+) : AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException,
+    ) {
+        val errorResponse = CommonApiResponse.error("인증에 실패했습니다.", HttpStatus.UNAUTHORIZED)
+        response.status = HttpServletResponse.SC_UNAUTHORIZED
+        response.contentType = "application/json"
+        response.writer.write(objectMapper.writeValueAsString(errorResponse))
+    }
+}
