0e837d21427a52927e4506c2563b3979cad43cfe
snowykte0426 <snowykte0426@naver.com>
2025-11-13 20:31:47 +0900

    update: 인증제 항목 엔티티에서 Nullable 설정 제거

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/category/dto/Category.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/category/dto/Category.kt
@@ -4,10 +4,10 @@ import com.team.incube.gsmc.v3.domain.category.dto.constant.EvidenceType
 
 data class Category(
     val id: Long,
-    val englishName: String?,
-    val koreanName: String?,
-    val weight: Int?,
-    val maximumValue: Int?,
-    val isAccumulated: Boolean?,
+    val englishName: String,
+    val koreanName: String,
+    val weight: Int,
+    val maximumValue: Int,
+    val isAccumulated: Boolean,
     val evidenceType: EvidenceType,
 )
--- src/main/kotlin/com/team/incube/gsmc/v3/domain/category/entity/CategoryExposedEntity.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/category/entity/CategoryExposedEntity.kt
@@ -5,11 +5,11 @@ import org.jetbrains.exposed.sql.Table
 
 object CategoryExposedEntity : Table(name = "tb_category") {
     val id = long(name = "category_id").autoIncrement()
-    val englishName = varchar(name = "category_english_name", length = 255).nullable()
-    val koreanName = varchar(name = "category_korean_name", length = 255).nullable()
-    val weight = integer(name = "weight").nullable()
-    val maximumValue = integer(name = "category_maximum_value").nullable()
-    val isAccumulated = bool(name = "is_accumulated").nullable()
+    val englishName = varchar(name = "category_english_name", length = 255)
+    val koreanName = varchar(name = "category_korean_name", length = 255)
+    val weight = integer(name = "weight")
+    val maximumValue = integer(name = "category_maximum_value")
+    val isAccumulated = bool(name = "is_accumulated")
     val evidenceType = enumeration<EvidenceType>(name = "evidence_type").default(EvidenceType.UNREQUIRED)
 
     override val primaryKey = PrimaryKey(id)
