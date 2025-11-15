64be0ab98c04bc7740500a6f2042ef424b55254a
snowykte0426 <snowykte0426@naver.com>
2025-11-15 23:37:46 +0900

    update: NCS 평균 점수 최소값 검증 강화

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/request/CreateNcsScoreRequest.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/request/CreateNcsScoreRequest.kt
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotNull
 
 data class CreateNcsScoreRequest(
     @param:Schema(description = "과목 전체 평균 점수", example = "4.6")
-    @field:Min(value = 0)
+    @field:Min(value = 1)
     @field:Max(value = 5)
     @field:NotNull
     val averageScore: Double,
