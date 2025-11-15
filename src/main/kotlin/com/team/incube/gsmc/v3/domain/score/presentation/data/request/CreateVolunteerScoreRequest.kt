ef5ba0d0b3690295f38e3aad526bf26580b9b86c
snowykte0426 <snowykte0426@naver.com>
2025-11-15 23:31:25 +0900

    update: 봉사 시간 입력 제약 완화

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/request/CreateVolunteerScoreRequest.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/request/CreateVolunteerScoreRequest.kt
@@ -1,14 +1,12 @@
 package com.team.incube.gsmc.v3.domain.score.presentation.data.request
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 
 data class CreateVolunteerScoreRequest(
     @param:Schema(description = "봉사 시간 (1시간당 1점, 최대 10점)", example = "5")
     @field:Min(value = 1)
-    @field:Max(value = 10)
     @field:NotNull
     val hours: Int,
 )
