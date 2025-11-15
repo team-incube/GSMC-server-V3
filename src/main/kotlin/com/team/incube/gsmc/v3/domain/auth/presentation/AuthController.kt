23ecc720f745668ba8ed0cf5d72b916910dc27ce
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-15 13:13:46 +0900

    add: 회원가입 API 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/presentation/AuthController.kt
@@ -1,9 +1,12 @@
 package com.team.incube.gsmc.v3.domain.auth.presentation
 
 import com.team.incube.gsmc.v3.domain.auth.presentation.data.request.OAuthCodeRequest
+import com.team.incube.gsmc.v3.domain.auth.presentation.data.request.SignUpRequest
 import com.team.incube.gsmc.v3.domain.auth.presentation.data.response.AuthTokenResponse
 import com.team.incube.gsmc.v3.domain.auth.service.OauthAuthenticationService
+import com.team.incube.gsmc.v3.domain.auth.service.SignUpService
 import com.team.incube.gsmc.v3.domain.auth.service.TokenRefreshService
+import com.team.incube.gsmc.v3.global.common.response.data.CommonApiResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
@@ -12,12 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.CookieValue
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Auth API", description = "인증 관리 API")
 @RestController
@@ -25,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(
     private val oauthAuthenticationService: OauthAuthenticationService,
     private val tokenRefreshService: TokenRefreshService,
+    private val signUpService: SignUpService,
 ) {
     @Operation(summary = "OAuth 인증", description = "Authentication Code를 통해 OAuth 인증을 처리합니다")
     @ApiResponses(
@@ -64,4 +63,26 @@ class AuthController(
         @Parameter(name = "refreshToken", `in` = ParameterIn.COOKIE, required = true, description = "Refresh token cookie")
         @CookieValue("refreshToken") refreshToken: String,
     ): AuthTokenResponse = tokenRefreshService.execute(refreshToken)
+
+    @Operation(summary = "회원가입", description = "회원가입합니다")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "202",
+                description = "회원가입 성공",
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "존재하지 않는 사용자임",
+                content = [Content()],
+            ),
+        ],
+    )
+    @PostMapping("signup")
+    fun signUp(
+        @Valid @RequestBody request: SignUpRequest,
+    ): CommonApiResponse<Nothing> {
+        signUpService.execute(request.name, request.studentNumber)
+        return CommonApiResponse.accepted("OK")
+    }
 }
---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/impl/SignUpServiceImpl.kt
@@ -0,0 +1,40 @@
+package com.team.incube.gsmc.v3.domain.auth.service.impl
+
+import com.team.incube.gsmc.v3.domain.auth.service.SignUpService
+import com.team.incube.gsmc.v3.domain.member.dto.constant.MemberRole
+import com.team.incube.gsmc.v3.domain.member.repository.MemberExposedRepository
+import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.stereotype.Service
+
+@Service
+class SignUpServiceImpl(
+    private val currentMemberProvider: CurrentMemberProvider,
+    private val memberExposedRepository: MemberExposedRepository,
+) : SignUpService {
+    private val log = org.slf4j.LoggerFactory.getLogger(SignUpServiceImpl::class.java)
+    override fun execute(
+        name: String,
+        studentNumber: Int,
+    ) {
+        transaction {
+            val member = currentMemberProvider.getCurrentUser()
+
+            val grade = studentNumber / 1000
+            val classNumber = (studentNumber / 100) % 10
+            val number = studentNumber % 100
+
+            log.info("$name grade $grade classNumber $classNumber number $number")
+
+            memberExposedRepository.update(
+                id = member.id,
+                name = name,
+                email = member.email,
+                grade = grade,
+                classNumber = classNumber,
+                number = number,
+                role = MemberRole.STUDENT,
+            )
+        }
+    }
+}
