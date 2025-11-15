2dfb5d8477b11b8fec5e42b8e48a0886aed09217
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-15 13:27:57 +0900

    update: 유효성 검사 어노테이션 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/presentation/data/request/SignUpRequest.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/presentation/data/request/SignUpRequest.kt
@@ -3,12 +3,13 @@ package com.team.incube.gsmc.v3.domain.auth.presentation.data.request
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 
 data class SignUpRequest(
     @field:Schema(description = "이름입니다.")
     @field:NotBlank(message = "이름은 필수입니다.")
     val name: String,
     @field:Schema(description = "학번입니다.")
-    @field:NotNull(message = "학번은 필수입니다.")
+    @field:Positive(message = "학번은 양수어야 합니다.")
     val studentNumber: Int,
 )
