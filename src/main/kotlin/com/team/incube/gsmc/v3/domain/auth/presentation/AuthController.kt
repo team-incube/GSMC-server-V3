da6d2b2b956d917fd0569b23a6f25102d93f62a1
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-15 13:32:13 +0900

    update: 와일드카드 제거

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/presentation/AuthController.kt
@@ -15,7 +15,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CookieValue
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "Auth API", description = "인증 관리 API")
 @RestController
