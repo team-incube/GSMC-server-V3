97787efd54bbc449ccaf12c3a90861cc9ff6afb6
C0ng_yun <s25069@gsm.hs.kr>
2025-11-16 22:43:33 +0900

    update: 사용자를 찾지 못했을 때 응답 제거

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/member/presentation/controller/MemberController.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/member/presentation/controller/MemberController.kt
@@ -85,11 +85,7 @@ class MemberController(
                     ),
                 ],
             ),
-            ApiResponse(
-                responseCode = "404",
-                description = "사용자를 찾을 수 없음",
-                content = [Content()],
-            ),
+
         ],
     )
     @SecurityRequirement(name = "bearerAuth")
