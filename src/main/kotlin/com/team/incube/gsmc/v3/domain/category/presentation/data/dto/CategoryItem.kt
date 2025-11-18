4735b11cd30ad706c0a015aebfc8412a2497dc11
snowykte0426 <snowykte0426@naver.com>
2025-11-18 12:16:08 +0900

    docs: Swagger 문서화에서 명칭 통일

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/category/presentation/data/dto/CategoryItem.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/category/presentation/data/dto/CategoryItem.kt
@@ -4,11 +4,11 @@ import com.team.incube.gsmc.v3.domain.category.constant.EvidenceType
 import com.team.incube.gsmc.v3.domain.category.constant.ScoreCalculationType
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "카테고리 응답")
+@Schema(description = "인증제 항목 응답")
 data class CategoryItem(
-    @field:Schema(description = "카테고리 영문명", example = "CERTIFICATE")
+    @field:Schema(description = "인증제 항목 영문명", example = "CERTIFICATE")
     val englishName: String,
-    @field:Schema(description = "카테고리 한글명", example = "자격증")
+    @field:Schema(description = "인증제 항목 한글명", example = "자격증")
     val koreanName: String,
     @field:Schema(description = "가중치", example = "2")
     val weight: Int?,
@@ -20,6 +20,6 @@ data class CategoryItem(
     val evidenceType: EvidenceType,
     @field:Schema(description = "점수 계산 타입", example = "COUNT_BASED")
     val calculationType: ScoreCalculationType,
-    @field:Schema(description = "외국어 카테고리 여부", example = "false")
+    @field:Schema(description = "외국어 인증제 항목 여부", example = "false")
     val isForeignLanguage: Boolean,
 )
--- src/main/kotlin/com/team/incube/gsmc/v3/domain/category/presentation/data/response/GetCategoriesResponse.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/category/presentation/data/response/GetCategoriesResponse.kt
@@ -3,8 +3,8 @@ package com.team.incube.gsmc.v3.domain.category.presentation.data.response
 import com.team.incube.gsmc.v3.domain.category.presentation.data.dto.CategoryItem
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "카테고리 검색 응답")
+@Schema(description = "인증제 항목 검색 응답")
 data class GetCategoriesResponse(
-    @field:Schema(description = "카테고리 목록")
+    @field:Schema(description = "인증제 항목 목록")
     val categories: List<CategoryItem>,
 )
