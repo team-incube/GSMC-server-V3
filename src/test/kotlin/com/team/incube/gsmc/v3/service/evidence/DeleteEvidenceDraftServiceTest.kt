27a23ae9003b84a78a649be7c821f8d1ccbbd79b
황지훈 <wangjajihooon@gmail.com>
2025-11-19 14:50:07 +0900

    test: 중복된 2개의 테스트 제거 및 Unit 검증 추가

--- src/test/kotlin/com/team/incube/gsmc/v3/service/evidence/DeleteEvidenceDraftServiceTest.kt
+++ src/test/kotlin/com/team/incube/gsmc/v3/service/evidence/DeleteEvidenceDraftServiceTest.kt
@@ -5,6 +5,7 @@ import com.team.incube.gsmc.v3.domain.member.dto.Member
 import com.team.incube.gsmc.v3.domain.member.dto.constant.MemberRole
 import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 
@@ -36,57 +37,11 @@ class DeleteEvidenceDraftServiceTest :
 
             every { c.currentMemberProvider.getCurrentMember() } returns member
 
-            When("임시저장을 삭제하면") {
-                c.service.execute()
+            When("execute를 호출하면") {
+                val result = c.service.execute()
 
-                Then("정상적으로 삭제된다") {
-                }
-            }
-        }
-
-        Given("다른 사용자의 임시저장 삭제 요청이 주어지면") {
-            val c = ctx()
-            val member =
-                Member(
-                    id = 100L,
-                    name = "김철수",
-                    email = "kim@gsm.hs.kr",
-                    grade = 1,
-                    classNumber = 3,
-                    number = 15,
-                    role = MemberRole.STUDENT,
-                )
-
-            every { c.currentMemberProvider.getCurrentMember() } returns member
-
-            When("임시저장을 삭제하면") {
-                c.service.execute()
-
-                Then("정상적으로 삭제된다") {
-                    // Cache eviction is triggered by @CacheEvict annotation
-                }
-            }
-        }
-
-        Given("선생님이 임시저장 삭제를 요청하면") {
-            val c = ctx()
-            val teacher =
-                Member(
-                    id = 999L,
-                    name = "선생님",
-                    email = "teacher@gsm.hs.kr",
-                    grade = 3,
-                    classNumber = 4,
-                    number = 1,
-                    role = MemberRole.TEACHER,
-                )
-
-            every { c.currentMemberProvider.getCurrentMember() } returns teacher
-
-            When("임시저장을 삭제하면") {
-                c.service.execute()
-
-                Then("정상적으로 삭제된다") {
+                Then("예외 없이 정상적으로 실행되고 캐시가 삭제된다") {
+                    result shouldBe Unit
                 }
             }
         }
