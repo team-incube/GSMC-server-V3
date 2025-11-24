aa2776ac7aa6cb09b2b4f3b6f69a7d23460a53a9
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-24 22:25:47 +0900

    update: alert 수정 메서드 호출부 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/alert/service/impl/PatchAlertIsReadServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/alert/service/impl/PatchAlertIsReadServiceImpl.kt
@@ -3,6 +3,7 @@ package com.team.incube.gsmc.v3.domain.alert.service.impl
 import com.team.incube.gsmc.v3.domain.alert.repository.AlertExposedRepository
 import com.team.incube.gsmc.v3.domain.alert.service.PatchAlertIsReadService
 import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,6 +12,13 @@ class PatchAlertIsReadServiceImpl(
     private val currentMemberProvider: CurrentMemberProvider,
 ) : PatchAlertIsReadService {
     override fun execute(lastAlertId: Long) {
-        val currentMember = currentMemberProvider.getCurrentMember()
+        transaction {
+            val currentMember = currentMemberProvider.getCurrentMember()
+
+            alertExposedRepository.updateIsReadTrueByReceiverIdAndLastAlertId(
+                receiverId = currentMember.id,
+                lastAlertId = lastAlertId
+            )
+        }
     }
 }
