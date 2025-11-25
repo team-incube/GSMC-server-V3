0d535d25564470d06bbf4a98399fe85f0919d725
snowykte0426 <snowykte0426@naver.com>
2025-11-25 19:46:47 +0900

    add: 레코드 카운팅 기반 점수 객체 수정 API 엔드포인트 정의

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/request/UpdateScoreWithValueAndFileRequest.kt
@@ -0,0 +1,17 @@
+package com.team.incube.gsmc.v3.domain.score.presentation.data.request
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+data class UpdateScoreWithValueAndFileRequest(
+    @param:Schema(
+        description = "점수/등급/이름 값",
+        example = "정보처리기사",
+    )
+    @field:NotBlank
+    val value: String,
+    @param:Schema(description = "증빙 파일 ID", example = "1")
+    @field:NotNull
+    val fileId: Long,
+)
