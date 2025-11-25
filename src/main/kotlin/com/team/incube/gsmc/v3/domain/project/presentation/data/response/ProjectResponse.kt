e8bec94273fe970fbb400639fd1a2e60b16d46d1
snowykte0426 <snowykte0426@naver.com>
2025-11-26 00:40:38 +0900

    update: ProjectResponse에서 participants와 scoreItems 필드 분리

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/project/presentation/data/response/ProjectResponse.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/project/presentation/data/response/ProjectResponse.kt
@@ -1,7 +1,8 @@
 package com.team.incube.gsmc.v3.domain.project.presentation.data.response
 
-import com.team.incube.gsmc.v3.domain.file.dto.File
-import com.team.incube.gsmc.v3.domain.project.dto.ProjectParticipant
+import com.team.incube.gsmc.v3.domain.file.presentation.data.dto.FileItem
+import com.team.incube.gsmc.v3.domain.member.dto.Member
+import com.team.incube.gsmc.v3.domain.score.presentation.data.dto.ScoreItem
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class ProjectResponse(
@@ -14,7 +15,9 @@ data class ProjectResponse(
     @param:Schema(description = "프로젝트 설명", example = "라즈베리파이를 활용한 스마트팜 자동화 시스템")
     val description: String,
     @param:Schema(description = "프로젝트 관련 파일 목록")
-    val files: List<File>,
-    @param:Schema(description = "프로젝트 참가자 목록 (scoreId 포함)")
-    val participants: List<ProjectParticipant>,
+    val files: List<FileItem>,
+    @param:Schema(description = "프로젝트 참가자 목록")
+    val participants: List<Member>,
+    @param:Schema(description = "프로젝트 관련 점수 목록")
+    val scoreItems: List<ScoreItem>,
 )
