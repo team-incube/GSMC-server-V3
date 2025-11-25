e1b53387bb1299bf9a3ab0f18d61e7925163f0a9
snowykte0426 <snowykte0426@naver.com>
2025-11-26 00:41:46 +0900

    update: 프로젝트 조회 서비스에서 점수 항목 및 파일 변환 로직 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/project/service/impl/FindMyProjectsServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/project/service/impl/FindMyProjectsServiceImpl.kt
@@ -1,30 +1,41 @@
 package com.team.incube.gsmc.v3.domain.project.service.impl
 
+import com.team.incube.gsmc.v3.domain.file.presentation.data.dto.FileItem
 import com.team.incube.gsmc.v3.domain.project.presentation.data.response.ProjectResponse
 import com.team.incube.gsmc.v3.domain.project.repository.ProjectExposedRepository
-import com.team.incube.gsmc.v3.domain.project.service.FindCurrentProjectsService
+import com.team.incube.gsmc.v3.domain.project.service.FindMyProjectsService
 import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Service
 
 @Service
-class FindCurrentProjectsServiceImpl(
+class FindMyProjectsServiceImpl(
     private val projectExposedRepository: ProjectExposedRepository,
     private val currentMemberProvider: CurrentMemberProvider,
-) : FindCurrentProjectsService {
+) : FindMyProjectsService {
     override fun execute(): List<ProjectResponse> =
         transaction {
             val currentUser = currentMemberProvider.getCurrentMember()
             val projects = projectExposedRepository.findProjectsByParticipantId(currentUser.id)
 
             projects.map { project ->
-                val scoreItems = projectExposedRepository.getProjectScoreItems(project.id!!)
+                val scoreItems = projectExposedRepository.findScoreItemsByProjectId(project.id!!)
+                val fileItems =
+                    project.files.map { file ->
+                        FileItem(
+                            id = file.id,
+                            member = file.member,
+                            originalName = file.originalName,
+                            storeName = file.storeName,
+                            uri = file.uri,
+                        )
+                    }
                 ProjectResponse(
                     id = project.id!!,
                     ownerId = project.ownerId,
                     title = project.title,
                     description = project.description,
-                    files = project.files,
+                    files = fileItems,
                     participants = project.participants,
                     scoreItems = scoreItems,
                 )
--- src/main/kotlin/com/team/incube/gsmc/v3/domain/project/service/impl/SearchProjectServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/project/service/impl/SearchProjectServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.team.incube.gsmc.v3.domain.project.service.impl
 
+import com.team.incube.gsmc.v3.domain.file.presentation.data.dto.FileItem
 import com.team.incube.gsmc.v3.domain.project.presentation.data.response.ProjectResponse
 import com.team.incube.gsmc.v3.domain.project.presentation.data.response.SearchProjectResponse
 import com.team.incube.gsmc.v3.domain.project.repository.ProjectExposedRepository
@@ -25,13 +26,25 @@ class SearchProjectServiceImpl(
 
             val projects =
                 projectPage.content.map { project ->
+                    val scoreItems = projectExposedRepository.findScoreItemsByProjectId(project.id!!)
+                    val fileItems =
+                        project.files.map { file ->
+                            FileItem(
+                                id = file.id,
+                                member = file.member,
+                                originalName = file.originalName,
+                                storeName = file.storeName,
+                                uri = file.uri,
+                            )
+                        }
                     ProjectResponse(
-                        id = project.id!!,
+                        id = project.id,
                         ownerId = project.ownerId,
                         title = project.title,
                         description = project.description,
-                        files = project.files,
+                        files = fileItems,
                         participants = project.participants,
+                        scoreItems = scoreItems,
                     )
                 }
 
