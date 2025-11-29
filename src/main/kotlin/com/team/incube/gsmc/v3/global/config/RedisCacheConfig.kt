27f6955ccd345bba362174ed79d6da3f2e4bf5db
snowykte0426 <snowykte0426@naver.com>
2025-11-29 15:22:03 +0900

    update: Redis 객체 직렬화 서브 타입 설정 적용

--- src/main/kotlin/com/team/incube/gsmc/v3/global/config/RedisCacheConfig.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/config/RedisCacheConfig.kt
@@ -1,7 +1,9 @@
 package com.team.incube.gsmc.v3.global.config
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.cache.annotation.EnableCaching
@@ -19,11 +21,24 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 class RedisCacheConfig {
     @Bean
     fun cacheManager(redisConnectionFactory: RedisConnectionFactory): RedisCacheManager {
+        val typeValidator =
+            BasicPolymorphicTypeValidator
+                .builder()
+                .allowIfSubType("com.team.incube.gsmc.v3")
+                .allowIfSubType("java.util")
+                .allowIfSubType("java.lang")
+                .build()
+
         val objectMapper =
             ObjectMapper().apply {
                 registerKotlinModule()
                 registerModule(JavaTimeModule())
                 disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                activateDefaultTyping(
+                    typeValidator,
+                    ObjectMapper.DefaultTyping.NON_FINAL,
+                    JsonTypeInfo.As.PROPERTY,
+                )
             }
 
         val redisCacheConfiguration =
