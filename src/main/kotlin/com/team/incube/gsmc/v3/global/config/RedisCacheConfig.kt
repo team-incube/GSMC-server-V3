d78de63bccf89c4f90d468c02cae293cedc2cdc4
snowykte0426 <snowykte0426@naver.com>
2025-11-29 15:48:21 +0900

    fix: 모든 타입에 `@class` 타입을 포함하도록 설정

--- src/main/kotlin/com/team/incube/gsmc/v3/global/config/RedisCacheConfig.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/global/config/RedisCacheConfig.kt
@@ -39,7 +39,7 @@ class RedisCacheConfig : CachingConfigurer {
                 disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 activateDefaultTyping(
                     typeValidator,
-                    ObjectMapper.DefaultTyping.NON_FINAL,
+                    ObjectMapper.DefaultTyping.EVERYTHING,
                     JsonTypeInfo.As.PROPERTY,
                 )
             }
