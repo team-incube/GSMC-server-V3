31c3bd64ead6d8e926c00df4c3ef8afae65923b8
snowykte0426 <snowykte0426@naver.com>
2025-11-30 14:24:49 +0900

    refactor: 프로젝트 삭제 시 S3 삭제를 이벤트 기반으로 변경

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/project/service/impl/DeleteProjectServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/project/service/impl/DeleteProjectServiceImpl.kt
@@ -7,9 +7,10 @@ import com.team.incube.gsmc.v3.domain.project.service.DeleteProjectService
 import com.team.incube.gsmc.v3.domain.score.repository.ScoreExposedRepository
 import com.team.incube.gsmc.v3.global.common.error.ErrorCode
 import com.team.incube.gsmc.v3.global.common.error.exception.GsmcException
+import com.team.incube.gsmc.v3.global.event.s3.S3BulkFileDeletionEvent
 import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
-import com.team.incube.gsmc.v3.global.thirdparty.aws.s3.service.S3DeleteService
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
@@ -19,7 +20,7 @@ class DeleteProjectServiceImpl(
     private val scoreExposedRepository: ScoreExposedRepository,
     private val evidenceExposedRepository: EvidenceExposedRepository,
     private val fileExposedRepository: FileExposedRepository,
-    private val s3DeleteService: S3DeleteService,
+    private val eventPublisher: ApplicationEventPublisher,
 ) : DeleteProjectService {
     override fun execute(projectId: Long) {
         transaction {
@@ -35,15 +36,14 @@ class DeleteProjectServiceImpl(
             val sourceIds = scores.mapNotNull { it.sourceId }
             val evidences = evidenceExposedRepository.findAllByIdIn(sourceIds)
             val allFiles = (evidences.flatMap { it.files } + project.files).distinctBy { it.id }
-            allFiles.forEach { file ->
-                s3DeleteService.execute(file.uri)
-            }
             val evidenceIds = evidences.map { it.id }
             evidenceExposedRepository.deleteAllByIdIn(evidenceIds)
             scoreExposedRepository.deleteAllByIdIn(scoreIds)
             projectExposedRepository.deleteProjectById(projectId)
             val fileIds = allFiles.map { it.id }
             fileExposedRepository.deleteAllByIdIn(fileIds)
+            val fileUris = allFiles.map { it.uri }
+            eventPublisher.publishEvent(S3BulkFileDeletionEvent(fileUris))
         }
     }
 }
