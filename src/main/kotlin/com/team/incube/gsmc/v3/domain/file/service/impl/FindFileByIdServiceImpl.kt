b0adf33a44da24df041ece5e09c9f6645835ddf1
snowykte0426 <snowykte0426@naver.com>
2025-12-02 21:26:08 +0900

    add: File 및 Member 단건 조회 API 구현
    - File 단건 조회 (소유자 권한 검증 포함)
      - GET /api/v3/files/{fileId}
      - GetFileResponse DTO 추가
      - FindFileByIdService 및 구현체 추가
      - FileController에 엔드포인트 추가
    
    - Member 단건 조회
      - GET /api/v3/members/{memberId}
      - FindMemberByIdService 및 구현체 추가
      - MemberController에 엔드포인트 추가

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/file/service/impl/FindFileByIdServiceImpl.kt
@@ -0,0 +1,36 @@
+package com.team.incube.gsmc.v3.domain.file.service.impl
+
+import com.team.incube.gsmc.v3.domain.file.presentation.data.response.GetFileResponse
+import com.team.incube.gsmc.v3.domain.file.repository.FileExposedRepository
+import com.team.incube.gsmc.v3.domain.file.service.FindFileByIdService
+import com.team.incube.gsmc.v3.global.common.error.ErrorCode
+import com.team.incube.gsmc.v3.global.common.error.exception.GsmcException
+import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.stereotype.Service
+
+@Service
+class FindFileByIdServiceImpl(
+    private val fileExposedRepository: FileExposedRepository,
+    private val currentMemberProvider: CurrentMemberProvider,
+) : FindFileByIdService {
+    override fun execute(fileId: Long): GetFileResponse =
+        transaction {
+            val file =
+                fileExposedRepository.findById(fileId)
+                    ?: throw GsmcException(ErrorCode.FILE_NOT_FOUND)
+
+            val currentMember = currentMemberProvider.getCurrentMember()
+
+            if (file.member != currentMember.id) {
+                throw GsmcException(ErrorCode.FILE_UNAUTHORIZED_ACCESS)
+            }
+            GetFileResponse(
+                id = file.id,
+                memberId = file.member,
+                originalName = file.originalName,
+                storeName = file.storeName,
+                uri = file.uri,
+            )
+        }
+}
