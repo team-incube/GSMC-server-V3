ef6b2d0433d3e3254456c28f52639f7af9f0a186
chaeeun09 <sgm20221207s@gsuite.gen.go.kr>
2025-12-08 00:25:59 +0900

    start.sh,stop.sh error

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,7 @@ set -e
 ECR_REGISTRY="PLACEHOLDER"
 ECR_REPOSITORY="gsmc-ecr"
 IMAGE_TAG="latest"
+APP_NAME="gsmc-app"
 
 echo "Logging in to ECR..."
 aws ecr get-login-password --region ap-northeast-2 | \
@@ -14,7 +15,7 @@ docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
 
 echo "Starting container..."
 docker run -d \
-  --name gsmc-app \
+  --name $APP_NAME \
   -p 8080:8080 \
   --restart unless-stopped \
   $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-APP_NAME="gsmc-application"
-APP_DIR="/home/ec2-user/gsmc-application"
+APP_NAME="gsmc-app"
 
-cd $APP_DIR
-
-docker-compose down || true
 docker stop $APP_NAME || true
 docker rm $APP_NAME || true
