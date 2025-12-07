12f93e6616608dd228694770633ef3f8a101ac64
chaeeun09 <sgm20221207s@gsuite.gen.go.kr>
2025-12-08 03:25:12 +0900

    Revert "nginx 및 codedeploy 연동 구조 수정"
    This reverts commit c8f87d70995892de135453a4ea1aa7acd1997253.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-ECR_REGISTRY="588738598492.dkr.ecr.ap-northeast-2.amazonaws.com"
+ECR_REGISTRY="PLACEHOLDER"
 ECR_REPOSITORY_NAME="gsmc-ecr"
 IMAGE_TAG="latest"
 APP_NAME="gsmc-application"
@@ -16,6 +16,3 @@ docker run -d \
   -p 8080:8080 \
   --restart unless-stopped \
   $ECR_REGISTRY/$ECR_REPOSITORY_NAME:$IMAGE_TAG
-
-# backend 실행 후 nginx reload
-sudo systemctl reload nginx
