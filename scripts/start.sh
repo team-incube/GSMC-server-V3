1465a25295acaff305d34baa796a7e82492f5950
chaeeun09 <sgm20221207s@gsuite.gen.go.kr>
2025-12-09 07:54:06 +0900

    docker-compose,appspec,start.sh 파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -ex
 
-ECR_REGISTRY="PLACEHOLDER"
+ECR_REGISTRY="588738598492.dkr.ecr.ap-northeast-2.amazonaws.com"
 ECR_REPOSITORY_NAME="gsmc-ecr"
 IMAGE_TAG="latest"
 APP_NAME="gsmc-application"
-
 aws ecr get-login-password --region ap-northeast-2 | \
-  docker login --username AWS --password-stdin $ECR_REGISTRY
-
-docker pull $ECR_REGISTRY/$ECR_REPOSITORY_NAME:$IMAGE_TAG
-
+  docker login --username AWS --password-stdin "$ECR_REGISTRY"
+IMAGE_URI="$ECR_REGISTRY/$ECR_REPOSITORY_NAME:$IMAGE_TAG"
+docker pull "$IMAGE_URI"
 docker run -d \
-  --name gsmc-application \
+  --name "$APP_NAME" \
   -p 8080:8080 \
   --restart unless-stopped \
-  -e SPRING_PROFILES_ACTIVE=dev \
-  588738598492.dkr.ecr.ap-northeast-2.amazonaws.com/gsmc-ecr:latest
+  -e SPRING_PROFILES_ACTIVE=prod \
+  "$IMAGE_URI"
