0ae8269f1f293f0ba719081558a7b09000ae4d2c
snowykte0426 <snowykte0426@naver.com>
2025-12-09 20:05:48 +0900

    update: `@gsm.hs.kr` 이메일 도메인만 인증 가능하도록 수정

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/impl/OAuthAuthenticationServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/impl/OAuthAuthenticationServiceImpl.kt
@@ -72,6 +72,10 @@ class OAuthAuthenticationServiceImpl(
                     ?: throw GsmcException(ErrorCode.AUTHENTICATION_FAILED)
             val name = (oauth2User.attributes["name"] as? String) ?: ""
 
+            if (email.endsWith("@gsm.hs.kr").not()) {
+                throw GsmcException(ErrorCode.AUTHENTICATION_FAILED)
+            }
+
             val member =
                 transaction {
                     memberExposedRepository.findByEmail(email)
