e99a93917c58f9aca853ba556d3eebdfaba03a15
snowykte0426 <snowykte0426@naver.com>
2025-12-10 14:09:35 +0900

    update: 점수 Request validation 메시지 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/request/UpdateScoreWithValueAndFileRequest.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/request/UpdateScoreWithValueAndFileRequest.kt
@@ -3,6 +3,7 @@ package com.team.incube.gsmc.v3.domain.score.presentation.data.request
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 
 data class UpdateScoreWithValueAndFileRequest(
     @param:Schema(
@@ -10,6 +11,7 @@ data class UpdateScoreWithValueAndFileRequest(
         example = "정보처리기사",
     )
     @field:NotBlank
+    @field:Size(max = 255, message = "값은 최대 255자까지 가능합니다")
     val value: String,
     @param:Schema(description = "증빙 파일 ID", example = "1")
     @field:NotNull
