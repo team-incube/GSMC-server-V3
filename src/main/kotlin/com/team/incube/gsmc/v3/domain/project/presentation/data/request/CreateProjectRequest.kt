c0632740e5591d961a75cfb2cd068fdf063f59d1
snowykte0426 <snowykte0426@naver.com>
2025-12-10 14:50:43 +0900

    update: 프로젝트 생성 시 예외 메세지 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/project/presentation/data/request/CreateProjectRequest.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/project/presentation/data/request/CreateProjectRequest.kt
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Size
 data class CreateProjectRequest(
     @param:Schema(description = "프로젝트 제목", example = "스마트팜 IoT 시스템")
     @field:NotBlank(message = "프로젝트 제목은 필수입니다")
-    @field:Size(min = 1, max = 100)
+    @field:Size(min = 1, max = 100, message = "프로젝트 제목은 최소 1자, 최대 100자까지 가능합니다")
     val title: String,
     @param:Schema(description = "프로젝트 설명", example = "라즈베리파이를 활용한 스마트팜 자동화 시스템")
     @field:NotBlank(message = "프로젝트 설명은 필수입니다")
