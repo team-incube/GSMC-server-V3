ce0407ed97e934bfe9635cd511b23a8356a14d7c
snowykte0426 <snowykte0426@naver.com>
2025-12-12 10:57:26 +0900

    update: CD 스크립트 내용을 올바르게 배치

--- scripts/prod/start.sh
+++ scripts/prod/start.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 set -ex
 
-ECR_REGISTRY="588738598492.dkr.ecr.ap-northeast-2.amazonaws.com"
-ECR_REPOSITORY_NAME="gsmc-ecr"
-IMAGE_TAG="latest"
-APP_NAME="gsmc-application"
+APP_DIR="/home/ec2-user/gsmc-application"
+cd $APP_DIR
+
+IMAGE_REGISTRY="588738598492.dkr.ecr.ap-northeast-2.amazonaws.com"
+REPO_NAME="gsmc-ecr"
+TAG="latest"
+IMAGE_URI="$IMAGE_REGISTRY/$REPO_NAME:$TAG"
+
+CONTAINER_NAME="gsmc-application"
+
 aws ecr get-login-password --region ap-northeast-2 | \
-  docker login --username AWS --password-stdin "$ECR_REGISTRY"
-IMAGE_URI="$ECR_REGISTRY/$ECR_REPOSITORY_NAME:$IMAGE_TAG"
-docker pull "$IMAGE_URI"
+  docker login --username AWS --password-stdin $IMAGE_REGISTRY
+
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+docker pull $IMAGE_URI
+
 docker run -d \
-  --name "$APP_NAME" \
+  --name $CONTAINER_NAME \
   -p 8080:8080 \
   --restart unless-stopped \
+  --env-file $APP_DIR/.env \
   -e SPRING_PROFILES_ACTIVE=prod \
-  "$IMAGE_URI"
+  $IMAGE_URI
