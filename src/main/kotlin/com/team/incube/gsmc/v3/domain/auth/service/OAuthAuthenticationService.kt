8f82149aaf197372cf35bf3f56b4eba83c37e486
snowykte0426 <snowykte0426@naver.com>
2025-12-21 19:41:47 +0900

    refactor: Service 인터페이스에 HttpServletResponse 파라미터 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/OAuthAuthenticationService.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/OAuthAuthenticationService.kt
@@ -1,7 +1,11 @@
 package com.team.incube.gsmc.v3.domain.auth.service
 
-import com.team.incube.gsmc.v3.domain.auth.dto.TokenPair
+import com.team.incube.gsmc.v3.domain.auth.presentation.data.response.AuthTokenResponse
+import jakarta.servlet.http.HttpServletResponse
 
 interface OAuthAuthenticationService {
-    fun execute(code: String): TokenPair
+    fun execute(
+        code: String,
+        response: HttpServletResponse,
+    ): AuthTokenResponse
 }
--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/TokenRefreshService.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/TokenRefreshService.kt
@@ -1,7 +1,11 @@
 package com.team.incube.gsmc.v3.domain.auth.service
 
-import com.team.incube.gsmc.v3.domain.auth.dto.TokenPair
+import com.team.incube.gsmc.v3.domain.auth.presentation.data.response.AuthTokenResponse
+import jakarta.servlet.http.HttpServletResponse
 
 interface TokenRefreshService {
-    fun execute(refreshToken: String): TokenPair
+    fun execute(
+        refreshToken: String,
+        response: HttpServletResponse,
+    ): AuthTokenResponse
 }
