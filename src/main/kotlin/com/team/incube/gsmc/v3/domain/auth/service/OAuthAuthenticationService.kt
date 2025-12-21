635368ba30cac8beed1dca4eaf518a5cb40c921a
snowykte0426 <snowykte0426@naver.com>
2025-12-21 23:28:30 +0900

    refactor: Service 인터페이스에서 HttpServletResponse 파라미터 제거

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/OAuthAuthenticationService.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/OAuthAuthenticationService.kt
@@ -1,11 +1,7 @@
 package com.team.incube.gsmc.v3.domain.auth.service
 
 import com.team.incube.gsmc.v3.domain.auth.presentation.data.response.AuthTokenResponse
-import jakarta.servlet.http.HttpServletResponse
 
 interface OAuthAuthenticationService {
-    fun execute(
-        code: String,
-        response: HttpServletResponse,
-    ): AuthTokenResponse
+    fun execute(code: String): AuthTokenResponse
 }
--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/TokenRefreshService.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/TokenRefreshService.kt
@@ -1,11 +1,7 @@
 package com.team.incube.gsmc.v3.domain.auth.service
 
 import com.team.incube.gsmc.v3.domain.auth.presentation.data.response.AuthTokenResponse
-import jakarta.servlet.http.HttpServletResponse
 
 interface TokenRefreshService {
-    fun execute(
-        refreshToken: String,
-        response: HttpServletResponse,
-    ): AuthTokenResponse
+    fun execute(refreshToken: String): AuthTokenResponse
 }
