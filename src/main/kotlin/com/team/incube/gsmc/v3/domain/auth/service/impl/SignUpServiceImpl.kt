72f2879b2899a7e40cd45caa4c52b57308681aef
snowykte0426 <snowykte0426@naver.com>
2025-12-23 15:09:23 +0900

    update: 조건식 단순화

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/impl/SignUpServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/auth/service/impl/SignUpServiceImpl.kt
@@ -55,16 +55,5 @@ class SignUpServiceImpl(
         grade: Int,
         classNumber: Int,
         number: Int,
-    ): Boolean {
-        // 학년: 1, 2, 3
-        if (grade !in 1..3) return false
-
-        // 반: 1, 2, 3, 4
-        if (classNumber !in 1..4) return false
-
-        // 번호: 1~18
-        if (number !in 1..18) return false
-
-        return true
-    }
+    ): Boolean = grade in 1..3 && classNumber in 1..4 && number in 1..18
 }
