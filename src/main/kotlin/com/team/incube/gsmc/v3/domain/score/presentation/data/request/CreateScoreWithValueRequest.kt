9c7dc0fc4064b8e2ca38e0f74aa5141c075f781f
snowykte0426 <snowykte0426@naver.com>
2025-12-29 11:56:58 +0900

    add: value만 받는 CreateScoreWithValueRequest DTO 추가

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/request/CreateScoreWithValueRequest.kt
@@ -0,0 +1,15 @@
+package com.team.incube.gsmc.v3.domain.score.presentation.data.request
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class CreateScoreWithValueRequest(
+    @param:Schema(
+        description = "점수/등급/이름 값",
+        example = "850",
+    )
+    @field:NotBlank
+    @field:Size(max = 255, message = "값은 최대 255자까지 가능합니다")
+    val value: String,
+)
