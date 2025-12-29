4c28fe952c71ea8c13b3fbbd3c44f969dd05b5e2
snowykte0426 <snowykte0426@naver.com>
2025-12-29 11:57:01 +0900

    update: 교과성적과 봉사시간을 학생이 직접 작성하도록 변경

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/score/service/impl/CreateVolunteerScoreServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/service/impl/CreateVolunteerScoreServiceImpl.kt
@@ -1,33 +1,23 @@
 package com.team.incube.gsmc.v3.domain.score.service.impl
 
-import com.team.incube.gsmc.v3.domain.alert.dto.constant.AlertType
 import com.team.incube.gsmc.v3.domain.category.constant.CategoryType
-import com.team.incube.gsmc.v3.domain.member.dto.constant.MemberRole
-import com.team.incube.gsmc.v3.domain.member.repository.MemberExposedRepository
 import com.team.incube.gsmc.v3.domain.score.presentation.data.response.CreateScoreResponse
 import com.team.incube.gsmc.v3.domain.score.repository.ScoreExposedRepository
 import com.team.incube.gsmc.v3.domain.score.service.BaseCreateOrUpdateBasedScoreService
 import com.team.incube.gsmc.v3.domain.score.service.CreateVolunteerScoreService
 import com.team.incube.gsmc.v3.global.common.error.ErrorCode
 import com.team.incube.gsmc.v3.global.common.error.exception.GsmcException
-import com.team.incube.gsmc.v3.global.event.alert.CreateAlertEvent
 import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class CreateVolunteerScoreServiceImpl(
     scoreExposedRepository: ScoreExposedRepository,
     currentMemberProvider: CurrentMemberProvider,
-    private val eventPublisher: ApplicationEventPublisher,
-    private val memberExposedRepository: MemberExposedRepository,
 ) : BaseCreateOrUpdateBasedScoreService(scoreExposedRepository, currentMemberProvider),
     CreateVolunteerScoreService {
-    override fun execute(
-        value: String,
-        memberId: Long,
-    ): CreateScoreResponse =
+    override fun execute(value: String): CreateScoreResponse =
         transaction {
             val intValue =
                 value.toIntOrNull()
@@ -37,35 +27,14 @@ class CreateVolunteerScoreServiceImpl(
                 throw GsmcException(ErrorCode.SCORE_VALUE_OUT_OF_RANGE)
             }
 
-            val student =
-                memberExposedRepository.findById(memberId)
-                    ?: throw GsmcException(ErrorCode.MEMBER_NOT_FOUND)
+            val student = currentMemberProvider.getCurrentMember()
 
-            val teacher = currentMemberProvider.getCurrentMember()
-
-            if (teacher.role == MemberRole.HOMEROOM_TEACHER) {
-                if (teacher.grade != student.grade || teacher.classNumber != student.classNumber) {
-                    throw GsmcException(ErrorCode.NOT_ASSIGNED_HOMEROOM_CLASS)
-                }
-            }
-
-            val score =
-                createOrUpdateScore(
-                    member = student,
-                    categoryType = CategoryType.VOLUNTEER,
-                    scoreValue = intValue.toDouble(),
-                    sourceId = null,
-                    isApprovedByDefault = true,
-                )
-
-            eventPublisher.publishEvent(
-                CreateAlertEvent(
-                    senderId = teacher.id,
-                    receiverId = student.id,
-                    scoreId = score.scoreId,
-                    alertType = AlertType.ADD_SCORE,
-                ),
+            createOrUpdateScore(
+                member = student,
+                categoryType = CategoryType.VOLUNTEER,
+                scoreValue = intValue.toDouble(),
+                sourceId = null,
+                isApprovedByDefault = false,
             )
-            score
         }
 }
