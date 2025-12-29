9e4f0d50159dd7ee61003309d5947dcfa6a8020c
snowykte0426 <snowykte0426@naver.com>
2025-12-29 12:19:22 +0900

    add: 교과성적/봉사시간 등록 시 담임 선생님에게 알림 발송 기능 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/score/service/impl/CreateAcademicGradeScoreServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/service/impl/CreateAcademicGradeScoreServiceImpl.kt
@@ -1,20 +1,27 @@
 package com.team.incube.gsmc.v3.domain.score.service.impl
 
+import com.team.incube.gsmc.v3.domain.alert.dto.constant.AlertType
 import com.team.incube.gsmc.v3.domain.category.constant.CategoryType
+import com.team.incube.gsmc.v3.domain.member.dto.constant.MemberRole
+import com.team.incube.gsmc.v3.domain.member.repository.MemberExposedRepository
 import com.team.incube.gsmc.v3.domain.score.presentation.data.response.CreateScoreResponse
 import com.team.incube.gsmc.v3.domain.score.repository.ScoreExposedRepository
 import com.team.incube.gsmc.v3.domain.score.service.BaseCreateOrUpdateBasedScoreService
 import com.team.incube.gsmc.v3.domain.score.service.CreateAcademicGradeScoreService
 import com.team.incube.gsmc.v3.global.common.error.ErrorCode
 import com.team.incube.gsmc.v3.global.common.error.exception.GsmcException
+import com.team.incube.gsmc.v3.global.event.alert.CreateAlertEvent
 import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class CreateAcademicGradeScoreServiceImpl(
     scoreExposedRepository: ScoreExposedRepository,
     currentMemberProvider: CurrentMemberProvider,
+    private val eventPublisher: ApplicationEventPublisher,
+    private val memberExposedRepository: MemberExposedRepository,
 ) : BaseCreateOrUpdateBasedScoreService(scoreExposedRepository, currentMemberProvider),
     CreateAcademicGradeScoreService {
     override fun execute(value: String): CreateScoreResponse =
@@ -29,12 +36,33 @@ class CreateAcademicGradeScoreServiceImpl(
 
             val student = currentMemberProvider.getCurrentMember()
 
-            createOrUpdateScore(
-                member = student,
-                categoryType = CategoryType.ACADEMIC_GRADE,
-                scoreValue = doubleValue,
-                sourceId = null,
-                isApprovedByDefault = false,
-            )
+            val score =
+                createOrUpdateScore(
+                    member = student,
+                    categoryType = CategoryType.ACADEMIC_GRADE,
+                    scoreValue = doubleValue,
+                    sourceId = null,
+                    isApprovedByDefault = false,
+                )
+
+            val homeroomTeachers =
+                memberExposedRepository.findByGradeAndClassNumberAndRole(
+                    grade = student.grade!!,
+                    classNumber = student.classNumber!!,
+                    role = MemberRole.HOMEROOM_TEACHER,
+                )
+
+            if (homeroomTeachers.isNotEmpty()) {
+                eventPublisher.publishEvent(
+                    CreateAlertEvent(
+                        senderId = student.id,
+                        receiverId = homeroomTeachers.first().id,
+                        scoreId = score.scoreId,
+                        alertType = AlertType.ADD_SCORE,
+                    ),
+                )
+            }
+
+            score
         }
 }
--- src/main/kotlin/com/team/incube/gsmc/v3/domain/score/service/impl/CreateVolunteerScoreServiceImpl.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/service/impl/CreateVolunteerScoreServiceImpl.kt
@@ -1,20 +1,27 @@
 package com.team.incube.gsmc.v3.domain.score.service.impl
 
+import com.team.incube.gsmc.v3.domain.alert.dto.constant.AlertType
 import com.team.incube.gsmc.v3.domain.category.constant.CategoryType
+import com.team.incube.gsmc.v3.domain.member.dto.constant.MemberRole
+import com.team.incube.gsmc.v3.domain.member.repository.MemberExposedRepository
 import com.team.incube.gsmc.v3.domain.score.presentation.data.response.CreateScoreResponse
 import com.team.incube.gsmc.v3.domain.score.repository.ScoreExposedRepository
 import com.team.incube.gsmc.v3.domain.score.service.BaseCreateOrUpdateBasedScoreService
 import com.team.incube.gsmc.v3.domain.score.service.CreateVolunteerScoreService
 import com.team.incube.gsmc.v3.global.common.error.ErrorCode
 import com.team.incube.gsmc.v3.global.common.error.exception.GsmcException
+import com.team.incube.gsmc.v3.global.event.alert.CreateAlertEvent
 import com.team.incube.gsmc.v3.global.security.jwt.util.CurrentMemberProvider
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class CreateVolunteerScoreServiceImpl(
     scoreExposedRepository: ScoreExposedRepository,
     currentMemberProvider: CurrentMemberProvider,
+    private val eventPublisher: ApplicationEventPublisher,
+    private val memberExposedRepository: MemberExposedRepository,
 ) : BaseCreateOrUpdateBasedScoreService(scoreExposedRepository, currentMemberProvider),
     CreateVolunteerScoreService {
     override fun execute(value: String): CreateScoreResponse =
@@ -29,12 +36,33 @@ class CreateVolunteerScoreServiceImpl(
 
             val student = currentMemberProvider.getCurrentMember()
 
-            createOrUpdateScore(
-                member = student,
-                categoryType = CategoryType.VOLUNTEER,
-                scoreValue = intValue.toDouble(),
-                sourceId = null,
-                isApprovedByDefault = false,
-            )
+            val score =
+                createOrUpdateScore(
+                    member = student,
+                    categoryType = CategoryType.VOLUNTEER,
+                    scoreValue = intValue.toDouble(),
+                    sourceId = null,
+                    isApprovedByDefault = false,
+                )
+
+            val homeroomTeachers =
+                memberExposedRepository.findByGradeAndClassNumberAndRole(
+                    grade = student.grade!!,
+                    classNumber = student.classNumber!!,
+                    role = MemberRole.HOMEROOM_TEACHER,
+                )
+
+            if (homeroomTeachers.isNotEmpty()) {
+                eventPublisher.publishEvent(
+                    CreateAlertEvent(
+                        senderId = student.id,
+                        receiverId = homeroomTeachers.first().id,
+                        scoreId = score.scoreId,
+                        alertType = AlertType.ADD_SCORE,
+                    ),
+                )
+            }
+
+            score
         }
 }
