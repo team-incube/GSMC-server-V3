f6b3f3113288318c777c4b3c8515d5e25be5707a
snowykte0426 <snowykte0426@naver.com>
2026-01-03 01:28:28 +0900

    add: 백분위수 조회 응답 DTO 추가

---
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/data/response/GetStudentPercentResponse.kt
@@ -0,0 +1,6 @@
+package com.team.incube.gsmc.v3.domain.score.presentation.data.response
+
+data class GetStudentPercentResponse(
+    val topPercentile: Double,
+    val bottomPercentile: Double,
+)
