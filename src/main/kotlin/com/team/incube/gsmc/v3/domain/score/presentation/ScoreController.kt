b7849996bb8e54750fe2ef73cd7f493d8756c919
snowykte0426 <snowykte0426@naver.com>
2026-01-03 01:29:42 +0900

    add: 백분위수 조회 API 엔드포인트 추가

--- src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/ScoreController.kt
+++ src/main/kotlin/com/team/incube/gsmc/v3/domain/score/presentation/ScoreController.kt
@@ -12,6 +12,7 @@ import com.team.incube.gsmc.v3.domain.score.presentation.data.response.CreateSco
 import com.team.incube.gsmc.v3.domain.score.presentation.data.response.GetMyScoresResponse
 import com.team.incube.gsmc.v3.domain.score.presentation.data.response.GetScoreResponse
 import com.team.incube.gsmc.v3.domain.score.presentation.data.response.GetScoresByCategoryResponse
+import com.team.incube.gsmc.v3.domain.score.presentation.data.response.GetStudentPercentResponse
 import com.team.incube.gsmc.v3.domain.score.presentation.data.response.GetTotalScoreResponse
 import com.team.incube.gsmc.v3.domain.score.service.ApproveScoreService
 import com.team.incube.gsmc.v3.domain.score.service.CalculateTotalScoreByMemberIdService
@@ -30,6 +31,8 @@ import com.team.incube.gsmc.v3.domain.score.service.CreateToeicScoreService
 import com.team.incube.gsmc.v3.domain.score.service.CreateTopcitScoreService
 import com.team.incube.gsmc.v3.domain.score.service.CreateVolunteerScoreService
 import com.team.incube.gsmc.v3.domain.score.service.DeleteScoreService
+import com.team.incube.gsmc.v3.domain.score.service.FindMyPercentInClassService
+import com.team.incube.gsmc.v3.domain.score.service.FindMyPercentInGradeService
 import com.team.incube.gsmc.v3.domain.score.service.FindMyScoresService
 import com.team.incube.gsmc.v3.domain.score.service.FindScoreByScoreIdService
 import com.team.incube.gsmc.v3.domain.score.service.FindScoresByCategoryByMemberIdService
@@ -88,6 +91,8 @@ class ScoreController(
     private val findScoreByScoreIdService: FindScoreByScoreIdService,
     private val findScoresByCategoryService: FindScoresByCategoryService,
     private val findScoresByCategoryByMemberIdService: FindScoresByCategoryByMemberIdService,
+    private val findMyPercentInClassService: FindMyPercentInClassService,
+    private val findMyPercentInGradeService: FindMyPercentInGradeService,
 ) {
     @Operation(summary = "인증제 점수 상태 업데이트", description = "인증제 점수의 승인/거절 상태를 업데이트합니다")
     @ApiResponses(
@@ -746,4 +751,40 @@ class ScoreController(
         value = request.value,
         fileId = request.fileId,
     )
+
+    @Operation(summary = "학급 내 백분위수 조회", description = "현재 인증된 사용자의 학급 내 상위/하위 백분위수를 조회합니다")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "요청이 성공함",
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "학년 또는 반 정보가 설정되지 않음",
+                content = [Content()],
+            ),
+        ],
+    )
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping("/percent/class")
+    fun getMyPercentInClass(): GetStudentPercentResponse = findMyPercentInClassService.execute()
+
+    @Operation(summary = "학년 내 백분위수 조회", description = "현재 인증된 사용자의 학년 내 상위/하위 백분위수를 조회합니다")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "요청이 성공함",
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "학년 정보가 설정되지 않음",
+                content = [Content()],
+            ),
+        ],
+    )
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping("/percent/grade")
+    fun getMyPercentInGrade(): GetStudentPercentResponse = findMyPercentInGradeService.execute()
 }
